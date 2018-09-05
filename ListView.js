b070b26d412155853b2b1bd01b0ded615f74f5b1
Bobby Lee <bobby.h.lee@gmail.com>
2018-09-05 22:40:33 +0700

    latest fixes

--- ListView.js
+++ ListView.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View, Image, FlatList, TouchableOpacity, Dimensions } from "react-native";
-import TEST_DATA from "../listingsArray.json";
+import TEST_DATA from "../listingArray.json";
+import styles from '../style/listViewStyle';
+import Card from '../Components/Card';
 
 export default class ListView extends Component {
   constructor(props) {
     super(props);
 
     this.state = {screen: Dimensions.get('window')};
-    // console.log(this.state.screen);
     this.onLayout = this.onLayout.bind(this);
   }
 
@@ -16,15 +17,14 @@ export default class ListView extends Component {
   }
 
   render() {
-    let screenWidth = this.state.screen.width - 30;
     return (
       <View style={styles.container} onLayout = {this.onLayout.bind(this)}>
          <FlatList
          style={styles.flatList}
-          data={TEST_DATA}
+          data={TEST_DATA.propertyList}
           keyExtractor={item => item.id.toString()}
           renderItem={({ item }) => (
-            <ListItem {...item} navigation={this.props.navigation} imageStyle={Object.assign({}, {width: screenWidth, height: (screenWidth * 9 / 16)})}/>
+            <Card {...item} navigation={this.props.navigation} />
           )}
         />
       </View>
@@ -32,50 +32,3 @@ export default class ListView extends Component {
   }
 }
 
-const ListItem = (props) => {
-  // console.log(props);
-  
-  return (
-    <TouchableOpacity onPress={()=> console.log('take me to PropertyDetailScreen')} style={styles.card}>
-      <Image
-        style={props.imageStyle}
-        source={{ uri: props.imgUrl[0] }}
-      />
-      <View style={styles.cardDesc}>
-        <Text style={styles.building}>{props.description.building_name}</Text>
-        <Text>{props.address.street} - {props.address.district} {props.address.city}</Text>
-        <Text>${props.description.price}/month | {props.description.bdrm} bdrm | {props.description.bdrm} bath | {props.description.size}</Text>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#ededed",
-    justifyContent: "center",
-    paddingTop: 20
-  },
-  flatList: {   
-    paddingLeft: 15,
-    paddingRight: 15
-  },
-  card: {
-    marginBottom: 20,
-    shadowOffset:{  width: 5,  height: 5,  },
-    shadowColor: '#ccc',
-    shadowOpacity: 0.7,
-  },
-  cardDesc: {
-    padding: 15,
-    backgroundColor: '#f7f7f7'
-  },
-  building: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 10
-  },
-  cardText: {
-    flex: 1,
-  }
-});
--- PropertyDetailScreen.js
+++ PropertyDetailScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Text, View, ScrollView, Image, StyleSheet, Dimensions, StatusBar, Animated } from "react-native";
-import TEST_DATA from "../listingsArray.json";
+import TEST_DATA from "../listingArray.json";
+import styles from '../style/propertyDetailStyle';
+import Carousel from '../Components/ImageCarousel';
 
 export default class PropertyDetailScreen extends Component {
   constructor(props) {
@@ -9,22 +11,23 @@ export default class PropertyDetailScreen extends Component {
       screen: Dimensions.get("window"),
       propertyList: []
     };
-    // console.log(this.state.screen);
     this.onLayout = this.onLayout.bind(this);
   }
 
   onLayout() {
     this.setState({ screen: Dimensions.get("window") });
+    console.log('this is onLayout screen dimensions ', this.state.screen);
+    
   }
 
   render() {
-    const { address, description, contact_info } = TEST_DATA[0];
+    const { address, description, contactInfo } = TEST_DATA.propertyList[0];
     return (
       <View style={styles.componentContainer} onLayout={this.onLayout}>
         <ScrollView vertical>
           <View style={styles.wrapCarousel}>
             <Carousel
-              images={TEST_DATA[0].imgUrl}
+              images={TEST_DATA.propertyList[0].imgUrl}
               imageStyle={{
                 width: this.state.screen.width,
                 height: (this.state.screen.width * 9) / 16
@@ -32,7 +35,7 @@ export default class PropertyDetailScreen extends Component {
             />
           </View>
           <View style={styles.wrapDescription}>
-            <Text style={styles.building}>{description.building_name}</Text>
+            <Text style={styles.building}>{description.buildingName}</Text>
             <Text style={styles.address}>
               {address.street} - {address.district} {address.city}
             </Text>
@@ -44,31 +47,33 @@ export default class PropertyDetailScreen extends Component {
               }}
             />
 
-            <View style={styles.propInfoWrap}>
-              <View style={styles.priceWrap}>
-                <Text style={styles.propInfo}>{`$ ${description.price}`}</Text>
-                <Text style={styles.propText}>Price</Text>
+            {//Property information group
+              <View style={styles.propInfoWrap}>
+                <View style={styles.priceWrap}>
+                  <Text style={styles.propInfo}>{`$ ${description.price}`}</Text>
+                  <Text style={styles.propText}>Price</Text>
+                </View>
+                <View style={styles.bedsWrap}>
+                  <Text style={styles.propInfo}>{`${description.bdrm}`}</Text>
+                  <Text style={styles.propText}>Beds</Text>
+                </View>
+                <View style={styles.bathWrap}>
+                  <Text style={styles.propInfo}>{`${description.bdrm}`}</Text>
+                  <Text style={styles.propText}>Baths</Text>
+                </View>
+                <View style={styles.sizeWrap}>
+                  <Text style={styles.propInfo}>{`${description.size}`}</Text>
+                  <Text style={styles.propText}>Sq. m</Text>
+                </View>
               </View>
-              <View sytle={styles.bedsWrap}>
-                <Text style={styles.propInfo}>{`${description.bdrm}`}</Text>
-                <Text style={styles.propText}>Beds</Text>
-              </View>
-              <View style={styles.bathWrap}>
-                <Text style={styles.propInfo}>{`${description.bdrm}`}</Text>
-                <Text style={styles.propText}>Baths</Text>
-              </View>
-              <View style={styles.sizeWrap}>
-                <Text style={styles.propInfo}>{`${description.size}`}</Text>
-                <Text style={styles.propText}>Sq. m</Text>
-              </View>
-            </View>
+            }
 
             <Text style={styles.descriptionText}>{description.text}</Text>
             <Text style={{ marginTop: 20, fontWeight: "bold" }}>
-              email: {contact_info.email}
+              Email: {contactInfo.email}
             </Text>
             <Text style={{ marginTop: 5, fontWeight: "bold" }}>
-              phone: {contact_info.phone_number}
+              Phone: {contactInfo.phoneNumber}
             </Text>
           </View>
         </ScrollView>
@@ -76,100 +81,3 @@ export default class PropertyDetailScreen extends Component {
     );
   }
 }
-
-class Carousel extends Component {
-  scrollX = new Animated.Value(0); 
-
-  render() {
-    const { width } = Dimensions.get("window");
-    let position = Animated.divide(this.scrollX, width);
-
-    return (
-      <View style={styles.carouselContainer}>
-        <ScrollView
-          horizontal
-          pagingEnabled
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: this.scrollX } } }] 
-          )} 
-          scrollEventThrottle={16} 
-          showsHorizontalScrollIndicator={false}
-        >
-          {this.props.images.map(image => (
-            <Image style={this.props.imageStyle} source={{ uri: image }} key={Math.random().toString(36).substr(2, 9)} />
-          ))}
-        </ScrollView>
-        <View style={{ flexDirection: "row" }} >
-          {this.props.images.map((_, i) => {
-            let opacity = position.interpolate({
-              inputRange: [i - 1, i, i + 1], 
-              outputRange: [0.3, 1, 0.3], 
-              extrapolate: "clamp" 
-            });
-            return (
-              <Animated.View key={i} style={{ opacity, height: 10, width: 10, backgroundColor: "#595959", margin: 8, borderRadius: 5 }} />
-            );
-          })}
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  componentContainer: {
-    flex: 1
-  },
-  carouselContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  wrapCarousel: {
-    flex: 1
-  },
-  wrapDescription: {
-    flex: 1,
-    padding: 15
-  },
-  building: {
-    marginBottom: 10,
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  address: {
-    fontSize: 16,
-    marginBottom: 20
-  },
-  propInfoWrap: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginBottom: 30
-  },
-  priceWrap: {
-    flex: 2
-  },
-  bedsWrap: {
-    flex: 1,
-    width: 150
-  },
-  bathWrap: {
-    flex: 1,
-    marginLeft: 30
-  },
-  sizeWrap: {
-    flex: 1,
-    marginLeft: 15
-  },
-  propInfo: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 10
-  },
-  propText: {
-    fontStyle: "italic"
-  },
-  descriptionText: {
-    fontSize: 18
-  }
-});
