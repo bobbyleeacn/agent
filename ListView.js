34d095fae33605429b4bc70ebeea2e1550b01cc8
Bobby Lee <bobby.h.lee@gmail.com>
2018-09-07 01:27:23 +0700

    subscription screen and listview toggle

--- ListView.js
+++ ListView.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, Image, FlatList, TouchableOpacity, Dimensions } from "react-native";
+import { Text, StyleSheet, View, Image, FlatList, TouchableOpacity, Dimensions, Button } from "react-native";
 import TEST_DATA from "../listingArray.json";
 import styles from '../style/listViewStyle';
 import Card from '../Components/Card';
+import { SearchBar } from 'react-native-elements';
+import SearchScreen from './SearchScreen.js';
 
 export default class ListView extends Component {
+  static navigationOptions = {
+    title: 'Search',
+    header: ({params}) => {
+      right:
+      <Button
+      title = "Test"
+      onPress = {() => console.log('button pressed')
+       } />
+    }
+  }
   constructor(props) {
     super(props);
+    this.state = {
+      screen: Dimensions.get('window'),
+      mapView: false
+    }
 
-    this.state = {screen: Dimensions.get('window')};
     this.onLayout = this.onLayout.bind(this);
   }
 
@@ -19,6 +34,12 @@ export default class ListView extends Component {
   render() {
     return (
       <View style={styles.container} onLayout = {this.onLayout.bind(this)}>
+      <View style={{paddingTop:30, marginLeft:10, flexDirection:'row'}} >
+        <SearchBar lightTheme clearIcon containerStyle={{width: 270}}  />
+        <View style={{marginLeft: 20}}><Button title={this.state.mapView ? 'List' : 'Map'} onPress={()=>this.setState({mapView: !this.state.mapView})}/>></View>
+      </View>
+
+      { this.state.mapView ? <SearchScreen /> :
          <FlatList
          style={styles.flatList}
           data={TEST_DATA.propertyList}
@@ -27,8 +48,8 @@ export default class ListView extends Component {
             <Card {...item} navigation={this.props.navigation} />
           )}
         />
+      }
       </View>
     );
   }
 }
-
